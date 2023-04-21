fa0945cb63ef7759f787b0b51de7e1444b8e2c1c
vimalds15 <antovimalands@gmail.com>
2023-04-21 21:53:00 +0530

    responsiveness fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,8 +23,9 @@ const Hero = () => {
           </p>
         </div>
       </div>
-
+      
       <ComputersCanvas />
+      
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -25,6 +25,7 @@ const EarthCanvas = () => {
         far: 200,
         position: [-4, 3, 6],
       }}
+      className="hidden md:block"
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
